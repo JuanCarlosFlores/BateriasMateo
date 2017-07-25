673acf370f88657c4eea8c2dd5e98679b5d45415
Juan Flores <juan.flores@synapse.com>
2017-07-24 22:00:57 -0300

    Save

--- Codigo/Java/JAVA/Mateo/src/main/java/com/bateriasMateo/controller/BateriaController.java
+++ Codigo/Java/JAVA/Mateo/src/main/java/com/bateriasMateo/controller/BateriaController.java
@@ -1,22 +1,28 @@
 package com.bateriasMateo.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bateriasMateo.domain.Bateria;
 import com.bateriasMateo.service.BateriaService;
 
 
 
 @Controller
-public class IndexController {
+public class BateriaController {
 	
 	@Autowired
 	BateriaService bateriaService;
 	
-	public IndexController(){
+	public BateriaController(){
 		super();
 	}
 	
@@ -30,6 +36,20 @@ public class IndexController {
 		model.addAttribute("bateria", bateriaService.get(id));
 		return "bateria/bateria";
 	}
-
-
+	@RequestMapping(value="/baterias/new")
+	public String newBateriaView(Model model){
+		model.addAttribute("bateria", new Bateria());
+	return "bateria/bateria";	
+	}
+	@RequestMapping(value="/baterias", method= RequestMethod.POST)
+	public String save(@Valid @ModelAttribute("bateria") Bateria bateria, BindingResult bindingResult, Model model){
+		if ((bindingResult.hasErrors())) {
+			model.addAttribute("bateria", bateria);			
+			return "bateria/bateria";
+		}else{
+			bateriaService.save(bateria);
+			return "redirect:bateria/bateria";
+		}
+	}
+	
 }
